221d97dfeb17616f4d922cf22b0f9c4bd8879c36
Piotr Piasecki <piterpiasecki@gmail.com>
2022-10-13 17:22:30 +0200

    ll

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 apt update
 apt install -y snapd
-snap install -y core
-snap refresh core
-snap install --classic certbot
-ln -s /snap/bin/certbot /usr/bin/certbot
-certbot --nginx
+snap --version
+# snap install core
+# snap refresh core
+# snap install --classic certbot
+# ln -s /snap/bin/certbot /usr/bin/certbot
+# certbot --nginx
